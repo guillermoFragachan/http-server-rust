3a07a334a8c97d3e87d70fc72996643e7c6f33c4
Guilermo Fragachan <guillefragachan@gmail.com>
2024-03-15 20:27:55 +0100

    new test

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,13 @@ use std::fs::File;
 use std::io::Read;
 use std::fs;
 use std::env;
+use std::path::{Path, PathBuf};
+use std::sync::{Once, OnceLock};
+
+use anyhow::Context;
 
 static CRLF: &str = "\r\n";
+static FILE_DIR: OnceLock<String> = OnceLock::new();
 
 fn print_directory_contents(directory_path: &str) {
     match fs::read_dir(directory_path) {
@@ -47,6 +52,15 @@ fn connect(mut _stream: TcpStream) {
     for line in lines.iter().skip(1) {
         let parts: Vec<_> = line.splitn(2, ": ").collect();
         headers.insert(parts[0].to_string(), parts[1].to_string());
+
+        let mut args = env::args().skip(1);
+    while let Some(arg) = args.next() {
+        if arg == "--directory" {
+            FILE_DIR
+                .set(args.next().context("no directory given!").expect("non"))
+                .unwrap();
+        }
+    }
     }
     match path {
         "/" => {
@@ -54,43 +68,22 @@ fn connect(mut _stream: TcpStream) {
             _stream.write(resp_status_line.as_bytes()).unwrap();
             _stream.write(CRLF.as_bytes()).unwrap();
         }
-        _ if path.starts_with("/files/") =>{
-            let resp_status_line = "HTTP/1.1 200 OK\r\n";
-            _stream.write(resp_status_line.as_bytes()).unwrap();
+        _ if path.starts_with("/file") =>{
+
             let content: Vec<&str> = path.split("files/").collect();
-
-
             let filename = content[1];
             let directory = env::args().nth(2).unwrap();
-            let file_name = format!("{directory}/{filename}");
-        
-            // print_directory_contents("./");
-        
-            // Check if the file exists in the local directory
-            let file_path = format!("./{}", file_name);
-            if let Ok(mut file) = File::open(file_path) {
-                // Set the content type based on the file extension (optional)
-                let content_type = match file_name.rsplit('.').next() {
-                    Some("txt") => "text/plain",
-                    Some("html") => "text/html",
-                    Some("css") => "text/css",
-                    Some("js") => "application/javascript",
-                    Some(_) => "application/octet-stream",
-                    None => "application/octet-stream",
-                };
-                _stream.write(format!("Content-Type: {}\r\n", content_type).as_bytes()).unwrap();
-        
-                // Read the file content and send it in the response
-                let mut file_content = Vec::new();
-                file.read_to_end(&mut file_content).unwrap();
-                _stream.write(format!("Content-Length: {}\r\n", file_content.len()).as_bytes()).unwrap();
-                _stream.write(CRLF.as_bytes()).unwrap();
-                _stream.write(&file_content).unwrap();
+            let filename = format!("{directory}/{filename}");
+            let mut response = String::new();
+            if Path::new(&filename).exists() {
+                let content = std::fs::read_to_string(filename).unwrap();
+                response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+                content.len(),
+                content
+            );
             } else {
-                // File not found, send a 404 response
-                let resp_status_line = "HTTP/1.1 404 Not Found\r\n";
-                _stream.write(resp_status_line.as_bytes()).unwrap();
-                _stream.write(CRLF.as_bytes()).unwrap();
+                response = "HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string();
             }
         }
         // starts with /echo
@@ -149,4 +142,15 @@ fn main() {
     }
 
     
+}
+
+
+fn send_bytes(stream: &mut TcpStream, bytes: &[u8]) -> anyhow::Result<()> {
+    write!(
+        stream,
+        "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n",
+        bytes.len(),
+    )
+    .context("failed to send bytes")?;
+    stream.write_all(bytes).context("failed to send bytes")
 }
